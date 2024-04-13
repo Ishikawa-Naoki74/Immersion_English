f6915925c531eef67bb0423bfaf5c9e837b2f41e
naoki74hub <nao.ryotaro.1112@icloud.com>
2024-04-13 23:30:17 +0900

    change entry point

--- view/asakatu-habit/vite.config.ts
+++ view/asakatu-habit/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
