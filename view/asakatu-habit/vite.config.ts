2b4161c42bfb1736f0081f5ca2a45da779ecd3e4
naoki74hub <nao.ryotaro.1112@icloud.com>
2024-04-14 22:36:42 +0900

    modify vite.config.ts

--- view/asakatu-habit/vite.config.ts
+++ view/asakatu-habit/vite.config.ts
@@ -3,19 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     VueDevTools(),
   ],
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      }
+      input: 'index.html'
     }
   },
   resolve: {
