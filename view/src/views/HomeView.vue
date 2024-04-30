e341e30a61ff1878960eea2ce0d2584b348bcd34
naoki74hub <nao.ryotaro.1112@icloud.com>
2024-04-30 21:19:49 +0900

    add login screen on HomeView

--- view/src/views/HomeView.vue
+++ view/src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <Login />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Login from "../components/Login.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+    Login
   },
 });
 </script>
