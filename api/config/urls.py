f709d09b511744e5e60605b7fc19cbfc8e5e70ee
naoki74hub <nao.ryotaro.1112@icloud.com>
2024-05-22 17:39:01 +0900

    add urls to every app

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,23 +1,19 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectacularRedocView,
+  SpectacularSwaggerView, 
+  )
+# プロジェクト全体の設定
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("immersion_english.urls")),
+    # localhost/8000/api/schema にアクセスするとテキストファイルをダウンロードできる
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # SwaggerUIの設定→localhost/8000/api/docs にアクセスするとSwaggerUIが表示されるよう設定
+    path("api/docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # Redocの設定→localhost/8000/api/redoc にアクセスするとRedocが表示されるよう設定
+    path("api/redoc", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/users/", include("apps.users.urls")),
 ]
+# TODO extend_schemaでドキュメンテーションの追加設定
